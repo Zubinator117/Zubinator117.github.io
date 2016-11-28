8184e5baae1cdc8dc0e957da6543d54f82e21c44
Zubin Havewala <zubinrh@gmail.com>
2016-11-28 22:06:24 +0000

    Thugify workshop

---
+++ Thugify/script.js
@@ -0,0 +1,6 @@
+function thugify() 
+{
+    var inputText = $('#text-input').val();
+    var thugifiedText = inputText + ", dawg";
+    document.getElementById('output').innerHTML = thugifiedText;
+}
